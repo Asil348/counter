3ded4c0d07b0fb920b06c9166f762527f3f6b014
Asil <asilefeakyol@hotmail.com>
2021-05-24 12:40:47 +0300

    settings modal functionality

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -1,6 +1,7 @@
 <template>
     <div :key="item.id" v-for="item in items">
-        <Item @add-count="$emit('add-count', item.id)" @subtr-count="$emit('subtr-count', item.id)" :item="item" />
+        <Item @add-count="$emit('add-count', item.id)" @subtr-count="$emit('subtr-count', item.id)"
+            @update-item="updateItem" :item="item" />
     </div>
 </template>
 
@@ -12,9 +13,14 @@
         props: {
             items: Array
         },
+        methods: {
+            updateItem(updItem) {
+                this.$emit('update-item', updItem)
+            }
+        },
         components: {
             Item
         },
-        emits: ['add-count', 'subtr-count']
+        emits: ['add-count', 'subtr-count', 'update-item']
     }
 </script>
